be3689413956f74b17ce0037ea3b087eafba9b45
AbdulQadir92 <a_qadir_jamali@hotmail.com>
2023-03-07 01:18:46 +0500

    Added functionality to save theme in localstorage even after page reloads

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useLayoutEffect } from "react";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 
@@ -13,8 +13,26 @@ import store from "../store/store";
 export default function App({ Component, pageProps }: AppProps) {
   const [isLight, setIsLight] = useState(true);
 
+  useLayoutEffect(() => {
+    const theme = localStorage.getItem('theme');
+    if (theme === null) {
+      localStorage.setItem('theme', 'light');
+    } else if (theme === 'light') {
+      setIsLight(true);
+    } else {
+      setIsLight(false);
+    }
+  }, [isLight])
+
   const toggleTheme = () => {
-    setIsLight(!isLight);
+    const theme = localStorage.getItem('theme');
+    if (theme === 'light') {
+      localStorage.setItem('theme', 'dark');
+      setIsLight(false);
+    } else {
+      localStorage.setItem('theme', 'light');
+      setIsLight(true);
+    }
   };
 
   return (
